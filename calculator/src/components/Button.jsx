5129c9eeec3490a8e01d08ae5470ca1de06f93fb
CuteCoder2 <fotsopires10@gmail.com>
2022-06-10 12:28:26 +0100

    initiali

---
+++ calculator/src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Buttons.module.css"
+
+function Button() {
+  return (
+
+    <button className={styles.Buttons}>click me</button>
+  )
+}
+
+export default Button
